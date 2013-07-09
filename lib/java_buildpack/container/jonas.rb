1da6aa5a274c14c01ec6c2772b39d661ee351755
Guillaume Berche <guillaume.berche@orange.com>
2013-07-09 11:42:57 +0200

    exports variables across bash cmds to have them propagated to child processes

--- lib/java_buildpack/container/jonas.rb
+++ lib/java_buildpack/container/jonas.rb
@@ -72,13 +72,14 @@ module JavaBuildpack::Container
       java_opts_string        = "JAVA_OPTS=\"#{ContainerUtils.to_java_opts_s(@java_opts)}\""
       deployme_var_string     = "JONAS_ROOT=#{JONAS_ROOT} JONAS_BASE=#{JONAS_BASE}"
       deployme_root = File.join JONAS_ROOT, 'deployme'
+      export_vars_string     = "export JONAS_ROOT JONAS_BASE JAVA_HOME JAVA_OPTS "
       topology_xml_file = File.join deployme_root, 'topology.xml'
       deployme_jar_file = File.join deployme_root, 'deployme.jar'
       topology_erb_cmd_string = "erb #{topology_xml_file}"
       deployme_cmd_string     = "java -jar #{deployme_jar_file} -topologyFile=#{topology_xml_file} -domainName=singleDomain -serverName=singleServerName"
       start_script_string     = File.join TOMCAT_HOME, 'bin', 'catalina.sh'
 
-      "#{java_home_string} #{java_opts_string} #{deployme_var_string};#{topology_erb_cmd_string} && #{deployme_cmd_string} && #{start_script_string} run"
+      "#{java_home_string} #{java_opts_string} #{deployme_var_string};#{export_vars_string};#{topology_erb_cmd_string} && #{deployme_cmd_string} && #{start_script_string} run"
     end
 
     private
